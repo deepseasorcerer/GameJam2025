d3b915cd5bcbecbd8cc33bd8e4278ca47af5490d
Brian McGrath <Champuster@gmail.com>
2025-02-16 15:35:47 -0500

    Starter Character controller

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -1,7 +1,29 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float gravity = 1f;
+    [SerializeField] private float lookSpeed = 2f;
+    [SerializeField] private float lookXLimit = 45f;
+
+    private float rotationX = 0;
+    private Vector3 _direction;
+    private CharacterController _controller;
+    private float _yVelocity;
+    
+    private Camera playerCamera;
+    
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        playerCamera = Camera.main;
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +33,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        float curSpeedX = Input.GetAxis("Vertical");
+        float curSpeedY = Input.GetAxis("Horizontal");
+        float movementDirectionY = _direction.y;
+        _direction = (forward * curSpeedX) + (right * curSpeedY);
+
         
+        #region rotation
+        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        #endregion
+        
+        _direction.Normalize();
+        _controller.Move(_direction * (speed * Time.deltaTime));
     }
 }
---
+++ Assets/Code/SoundManager.cs
@@ -0,0 +1,76 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    private AudioSource _source;
+    [Range(0f, 1f)]
+    public float volume = 0.7f;
+    public bool loop = false;
+    [Range(0f, 1.5f)]
+    public float pitch = 1f;
+    [Range(0f, 0.5f)]
+    public float volumeRandomness = 0.1f;
+    [Range(0f, 0.5f)]
+    public float pitchRandomness = 0.1f;
+    public void SetSource(AudioSource source)
+    {
+        _source = source;
+        _source.clip = clip;
+    }
+
+    public void Play()
+    {
+        _source.volume = volume * (1 + Random.Range(-volumeRandomness / 2f, volumeRandomness / 2f));
+        _source.loop = loop;
+        _source.pitch = pitch * (1 + Random.Range(-pitchRandomness / 2f, pitchRandomness / 2f));
+        _source.Play();
+    }
+}
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("More than one SoundManager in the scene");
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    
+    [SerializeField] private Sound[] sounds;
+
+    private void Start()
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject _go = new GameObject("Sound_" + i + "_" + sounds[i].name);
+            _go.transform.SetParent(this.transform);
+            sounds[i].SetSource(_go.AddComponent<AudioSource>());
+        }
+    }
+    
+    public void PlaySound(string soundName)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == soundName)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+        Debug.LogWarning("SoundManager: Sound not found in list, " + soundName);
+    }
+}
