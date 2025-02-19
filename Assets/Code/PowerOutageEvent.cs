87b0d6bc1da7d0feeb1ba39937ed4694a7c0d9d0
Brian McGrath <Champuster@gmail.com>
2025-02-19 01:27:30 -0500

    Models and placeholder events

---
+++ Assets/Code/PowerOutageEvent.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+
+public class PowerOutageEvent : MonoBehaviour
+{
+    public void ActivateTask()
+    {
+        Debug.Log("Power outage event activated");
+    }
+}
---
+++ Assets/Code/RedirectShipEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class RedirectShipEvent : MonoBehaviour
+    {
+        public void ActivateTask()
+        {
+            Debug.Log("Redirect ship event activated");
+        }
+        
+    }
+}
