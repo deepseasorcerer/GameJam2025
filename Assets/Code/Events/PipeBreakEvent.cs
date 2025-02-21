d6bdbe7153e17ddcc7359b03a11e6cbcabb5a12c
Brian McGrath <Champuster@gmail.com>
2025-02-21 01:43:15 -0500

    init

--- Assets/Code/Events/PipeBreakEvent.cs
+++ Assets/Code/Events/PipeBreakEvent.cs
@@ -3,13 +3,56 @@
 
 public class PipeBreakEvent: InteractableBase
 {
+    [SerializeField] string GameEventName = "PipeBreak";
+    [SerializeField] private GameEventType eventType = GameEventType.PipeBreak;
+    [SerializeField] private float EventDuration = 60f;
+    [SerializeField] private bool isFixed = false;
+    public float timeLeft;
+    private SoundManager _soundManager;
+
+    private void Start()
+    {
+        _soundManager = SoundManager.Instance;
+        if (_soundManager == null)
+        {
+            Debug.LogError("SoundManager is null");
+        }
+    }
+    
+    void Update()
+    {
+        if (isActive)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft <= 0)
+            {
+                FailedTask();
+            }
+        }
+    }
+    
+    public void FailedTask()
+    {
+        isActive = false;
+        Debug.Log("Pipebreak event failed");
+    }
+
     public void ActivateTask()
     {
-        Debug.Log("Fire event activated");
+        _soundManager.PlaySound("PowerOutage");
+        isActive = true;
+        isFixed = false;
+        timeLeft = EventDuration;
     }
 
     protected override void PerformInteraction()
     {
-        throw new System.NotImplementedException();
+        if (isActive)
+        {
+            isFixed = true;
+            isActive = false;
+            _soundManager.PlaySound("PowerOn");
+            Debug.Log("Power Outage event fixed");
+        }
     }
 }
